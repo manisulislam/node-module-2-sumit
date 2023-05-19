acc54e9971bd0e564c936d9b90b6d3472d2409b8
anisul islam <anisulislam980340@gmail.com>
2023-05-19 10:57:29 +0600

    path module completed

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const path= require("path");
 const mypath= 'E:/node/node-express-mongodb-freecodecamp/module-two/index.js'
 
 console.log(path.basename(mypath));
+console.log(path.dirname(mypath));
+console.log(path.extname(mypath));
+console.log(path.parse(mypath));
